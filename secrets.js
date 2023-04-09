5a1d260be54951496b442cf8837ce5ad082d5820
Sergio Gomez C <segoco@gmail.com>
2023-04-08 21:53:53 -0500

    appi keys added

--- secrets.js
+++ secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiKey: "0a25d932dc9655c3f60aa04d955a4a36-us14",
-  listId: "f41e130261",
-  dataCenter: "us14",
+  apiKey: '0a25d932dc9655c3f60aa04d955a4a36-us14',
+  listId: 'f41e130261'
+  dataCenter: 'us14',
 };
